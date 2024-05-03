e3b9e0247aad3386c77f50ca3eef6f6d5c94e095
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-05-03 10:53:51 +0600

    DotBackgroundMain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
 import { AuroraBackgroundMain } from '@/components/AuroraBackgroundMain';
 import { BackgroundBeamsMain } from '@/components/BackgroundBeamsMain';
+import { BackgroundBoxesMain } from '@/components/BackgroundBoxesMain';
+import { BackgroundGradientAnimationMain } from '@/components/BackgroundGradientAnimationMain';
+import { BackgroundGradientMain } from '@/components/BackgroundGradientMain';
+import { BentoGridThirdMain } from '@/components/BentoGridThirdMain';
+import { CanvasRevealEffectMain } from '@/components/CanvasRevealEffectMain';
+import { CardHoverEffectMain } from '@/components/CardHoverEffectMain';
+import { DotBackgroundMain } from '@/components/DotBackgroundMain';
+import { EvervaultCardMain } from '@/components/EvervaultCardMain';
+import { GoogleGeminiEffectMain } from '@/components/GoogleGeminiEffectMain';
 import { HeroParallaxParent } from '@/components/HeroParallaxParent';
 import { BackgroundBeams } from '@/components/ui/background-beams';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='bg-black'>
+      <BackgroundBeams />
       <HeroParallaxParent/>
       <AuroraBackgroundMain/>
       <BackgroundBeamsMain/>
-      <BackgroundBeams />
+      <BackgroundBoxesMain/>
+      <BackgroundGradientMain/>
+      <BentoGridThirdMain/>
+      <CanvasRevealEffectMain/>
+      <CardHoverEffectMain/>
+      <EvervaultCardMain/>
+      <GoogleGeminiEffectMain/>
+      <BackgroundGradientAnimationMain/>
+      <DotBackgroundMain/>
+  
     </div>
   );
 };
