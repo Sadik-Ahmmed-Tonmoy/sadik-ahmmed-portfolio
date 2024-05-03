0a32d8bf6a9f2cc964f11834afc23466d29fe3af
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-05-03 09:52:07 +0600

    background-beams.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { AuroraBackgroundMain } from '@/components/AuroraBackgroundMain';
+import { BackgroundBeamsMain } from '@/components/BackgroundBeamsMain';
 import { HeroParallaxParent } from '@/components/HeroParallaxParent';
+import { BackgroundBeams } from '@/components/ui/background-beams';
 
 const HomePage = () => {
   return (
     <div>
       <HeroParallaxParent/>
       <AuroraBackgroundMain/>
+      <BackgroundBeamsMain/>
+      <BackgroundBeams />
     </div>
   );
 };
